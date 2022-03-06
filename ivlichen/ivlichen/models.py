605f4beaca1100bfafb6212e2708ec77b38066f1
NicolasSchwab <nicolas.schwab@ceff.ch>
2022-03-06 19:12:04 +0100

    Change models

--- ivlichen/ivlichen/models.py
+++ ivlichen/ivlichen/models.py
@@ -2,21 +2,18 @@ from django.db import models
 from django.db.models import Model
 
 
-class Taxon(Model):
-    name = models.ForeignKey('Name', on_delete=models.CASCADE)
-    cover_image = models.ForeignKey('Image', on_delete=models.CASCADE)
-
-
-class Name(Model):
+class Species(Model):
     name = models.CharField(max_length=100)
     authors = models.CharField(max_length=100)
     publication_year = models.CharField(max_length=50)
+    cover_image = models.ForeignKey('Image', on_delete=models.CASCADE)
 
 
 class Image(Model):
-    image = models.ImageField('/media/taximg/')
+    image = models.ImageField('taximg/')
+    legend = models.CharField(max_length=125)
 
 
 class Observation(Model):
-    taxon = models.ForeignKey('Taxon', models.CASCADE)
-    images = models.OneTo('Image', models.CASCADE)
+    taxon = models.ForeignKey('Species', models.CASCADE)
+    images = models.ManyToManyField('Image', models.CASCADE)
